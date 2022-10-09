8c871cd0732e909ebefc3d22bce16b01480af86e
Dahaka_Develop <typeboolean46@gmail.com>
2022-10-09 18:32:58 +0300

    PreReliase_For_Lexer

--- lexer/basic.py
+++ lexer/basic.py
@@ -1,110 +1,150 @@
-#Types that we are going to have TYPES, defined as constants
-#CONSTANTS
+#######################################
+# CONSTANTS
+#######################################
 
+DIGITS = '0123456789'
 
-DIGITS  = '0123456789'
-
-#Errors
+#######################################
+# ERRORS
+#######################################
 
 class Error:
-    def __init__ (self, error_name, details):
+    def __init__(self, pos_start, pos_end, error_name, details):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
         self.error_name = error_name
         self.details = details
+    
     def as_string(self):
-        result = f'{self.error_name}: {self.details}'
+        result  = f'{self.error_name}: {self.details}\n'
+        result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
         return result
-class IllegalCharError(Error):
-    def __init__(self, details):
-        super().__init__('Illefal Character', details)
 
-#TOKENS
- 
-TT_INT        = 'TT_INT'
-TT_FLOAT      = 'FLOAT'
-TT_PLUS       = 'PLUS'  
-TT_MINUS      = 'MINUS'
-TT_MUL        = 'MUL' 
-TT_LBRACKET   = 'LBRACKET'
-TT_RBRACKET   = 'RBRACKET'
-TT_DIV        = 'DIV'
+class IllegalCharError(Error):
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Illegal Character', details)
+
+#######################################
+# POSITION
+#######################################
+
+class Position:
+    def __init__(self, idx, ln, col, fn, ftxt):
+        self.idx = idx
+        self.ln = ln
+        self.col = col
+        self.fn = fn
+        self.ftxt = ftxt
+
+    def advance(self, current_char):
+        self.idx += 1
+        self.col += 1
+
+        if current_char == '\n':
+            self.ln += 1
+            self.col = 0
+
+        return self
+
+    def copy(self):
+        return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
+
+#######################################
+# TOKENS
+#######################################
+
+TT_INT		= 'INT'
+TT_FLOAT    = 'FLOAT'
+TT_PLUS     = 'PLUS'
+TT_MINUS    = 'MINUS'
+TT_MUL      = 'MUL'
+TT_DIV      = 'DIV'
+TT_LPAREN   = 'LPAREN'
+TT_RPAREN   = 'RPAREN'
 
 class Token:
-    def __init__(self, type_, value = None): #initialisation method
-      self.type = type_
-      self.value = value
-
-    def __repr__(self):  #representational method
-       if self.value: return f'{self.type}:{self.value}' #if value is given prints a value and a type
-       return f'{self.type}'#if value is't given prints a type
-
-#LEXER
-
-class Lexer: #initial method (based)
-    def __init__(self, text): #text that will be processing
-       self.text  = text 
-       self.pos  = -1 #tracking of current position and character
-       self.current_char = None
+    def __init__(self, type_, value=None):
+        self.type = type_
+        self.value = value
     
-    def advance(self): #advance method that drives to the next character in the text 
-        self.pos += 1
-        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None #increasing a position of text until the text will end, than tags it as None
+    def __repr__(self):
+        if self.value: return f'{self.type}:{self.value}'
+        return f'{self.type}'
+
+#######################################
+# LEXER
+#######################################
+
+class Lexer:
+    def __init__(self, fn, text):
+        self.fn = fn
+        self.text = text
+        self.pos = Position(-1, 0, -1, fn, text)
+        self.current_char = None
+        self.advance()
+    
+    def advance(self):
+        self.pos.advance(self.current_char)
+        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
 
     def make_tokens(self):
-        tokens =[]
+        tokens = []
 
-        while self.current_char != None: #for token that is taged as None
-            if self.current_char in '\t': #if meets space
-                self.advance() #jump to next charracter
+        while self.current_char != None:
+            if self.current_char in ' \t':
+                self.advance()
             elif self.current_char in DIGITS:
-                tokens.append(self.make_number())#we have to find a way how to define is number integer or not
-            elif self.current.char =='+':#if meets plus symbol
+                tokens.append(self.make_number())
+            elif self.current_char == '+':
                 tokens.append(Token(TT_PLUS))
                 self.advance()
-            elif self.current.char =='-':#if meets minus symbol
-                tokens.append(Token(TT_PLUS))
+            elif self.current_char == '-':
+                tokens.append(Token(TT_MINUS))
                 self.advance()
-            elif self.current.char =='/':#if meets divide symbol
-                tokens.append(Token(TT_DIV))
-                self.advance()
-            elif self.current.char =='*':#if meets mul symbol
+            elif self.current_char == '*':
                 tokens.append(Token(TT_MUL))
                 self.advance()
-            elif self.current.char =='(':#if meets mul symbol
-                tokens.append(Token(TT_LBRACKET))
+            elif self.current_char == '/':
+                tokens.append(Token(TT_DIV))
                 self.advance()
-            elif self.current.char ==')':#if meets mul symbol
-                tokens.append(Token(TT_RBRACKET))
+            elif self.current_char == '(':
+                tokens.append(Token(TT_LPAREN))
+                self.advance()
+            elif self.current_char == ')':
+                tokens.append(Token(TT_RPAREN))
                 self.advance()
             else:
+                pos_start = self.pos.copy()
                 char = self.current_char
                 self.advance()
-                return [], IllegalCharError("'" + char + "'")
+                return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
 
+        return tokens, None
 
-            return tokens, None    
+    def make_number(self):
+        num_str = ''
+        dot_count = 0
 
-        def make_number(self):
-            num_str = '' #to keep track of number in the string
-            dot_count = 0 #dot tracking if number contsins a dot it means that it is as float type
+        while self.current_char != None and self.current_char in DIGITS + '.':
+            if self.current_char == '.':
+                if dot_count == 1: break
+                dot_count += 1
+                num_str += '.'
+            else:
+                num_str += self.current_char
+            self.advance()
 
-            while self.current_char != None and self.current_char in DIGITS + '.':
-                if self.current_char == '.':
-                    if dot_count == 1 : break #we can't have two dots in a single number
-                    dot_count += 1
-                    num_str += '.'
-                else: #if it's not a dash 
-                    num_str += self.current_char #adding string to current meaning  
-                self.advance()
-                    
-        if self.dot_count == 0:
-            return Token(TT_INT, int(self.num_str)) #transforming string into int
+        if dot_count == 0:
+            return Token(TT_INT, int(num_str))
         else:
-            return Token(TT_FLOAT, float(self.num_str)) #transforming string into float   
+            return Token(TT_FLOAT, float(num_str))
 
-#RUN
+#######################################
+# RUN
+#######################################
 
-def run(text): #function which is going to take text and run it 
-    lexer = Lexer(text)
+def run(fn, text):
+    lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
+
     return tokens, error
-#have to be upgraded with new steps in the future
--- lexer/shell.py
+++ lexer/shell.py
@@ -1,10 +1,8 @@
-from cgitb import text
-from distutils.log import error
 import basic
 
-while True: 
-    text = input('basic >')
-    result, error = basic.run(text)
+while True:
+    text = input('basic > ')
+    result, error = basic.run('<stdin>', text)
 
     if error: print(error.as_string())
     else: print(result)
